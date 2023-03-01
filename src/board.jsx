ea0eb0754465ad7434314c85988f02069b899911
Daniel Peacock <dpeacock@umich.edu>
2023-03-01 14:22:46 -0500

    fixed lose logic

--- src/board.jsx
+++ src/board.jsx
@@ -283,7 +283,11 @@ function Board() {
         }//for
         //edge case: edges===0 and bottom right corner
         if(board[3][3] === board[3][2]
+            || board[3][2] === board[3][1]
+            || board[3][1] === board[3][0]
             || board[3][3] === board[2][3]
+            || board[2][3] === board[1][3] 
+            || board[1][3] === board[0][3] 
             || board[3][3] === 0
             || board[3][2] === 0
             || board[3][1] === 0
